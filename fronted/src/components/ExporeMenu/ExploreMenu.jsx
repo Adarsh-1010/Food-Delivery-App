616b19e22f3fa292d7160bbd84e9ebaa1fd32f88
Adarsh-1010 <adarsh.21ug3009@iiitranchi.ac.in>
2025-03-15 01:58:29 +0530

    components assets pages done

---
+++ fronted/src/components/ExporeMenu/ExploreMenu.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './ExploreMenu.css'
+import { menu_list } from '../../assets/frontend_assets/assets'
+
+const ExploreMenu = () => {
+  return (
+    <div>
+      <div className="explore-menu" id="explore-menu">
+        <h1>Explore our menu</h1>
+        <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingradients and culinary expertise.Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time</p>
+      </div>
+    </div>
+  )
+}
+
+export default ExploreMenu
