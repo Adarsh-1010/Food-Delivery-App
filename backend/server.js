1a2eb8d2612b0343b0af9c49b13b7304de43b68f
Adarsh-1010 <adarsh.21ug3009@iiitranchi.ac.in>
2025-03-25 20:32:44 +0530

    setup the backend server and mongoDB

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+import { connectDb } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+
+
+
+
+
+//App Config:
+const app=express();
+const port=4000
+
+//Middleware:
+app.use(express.json());
+app.use(cors());
+
+//DB connection:
+connectDb();
+
+//api endpoints
+app.use("/api/food",foodRouter);
+
+
+
+app.get("/",(req,res)=>{
+    res.send("Api working");
+});
+
+app.listen(port,()=>{
+    console.log(`Server started on http://localhost:${port}`);
+});
+
+// mongodb+srv://adarshpathak181210:<db_password>@cluster0.6d19m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+ 
